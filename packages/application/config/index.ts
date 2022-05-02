67fe8cea9a3d4de6c1d10de682b83c3ed604ae86
David Mazzitelli <davidma1984@gmail.com>
2022-05-02 17:35:38 -0300

    Update index.ts

--- packages/application/config/index.ts
+++ packages/application/config/index.ts
@@ -4,6 +4,6 @@ export interface IConfig {
 }
 
 export default {
-  donationAddressPOI: process.env.NEXT_PUBLIC_POI_ADDRESS || '',
+  donationAddressPOI: process.env.NEXT_PUBLIC_POI_DONATION_ADDRESS || '',
   validChainId: parseInt(process.env.NEXT_PUBLIC_CHAIN_ID || '', 10) || 1
 } as IConfig
