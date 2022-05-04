dabeedbb9fc6c957df77878ad0bc9ad04a6d9e73
David Mazzitelli <davidma1984@gmail.com>
2022-05-04 15:42:03 -0300

    Removed content of index page

--- packages/application/pages/index.tsx
+++ packages/application/pages/index.tsx
@@ -1,54 +1,17 @@
-import { Flex, VStack, Text } from 'native-base'
+import { Text } from 'native-base'
 import type { NextPage } from 'next'
-import Link from 'next/link'
-import Router from 'next/router'
 
-import { DollarSignIcon, HandShakeIcon } from '@components/atoms'
-import { ActionCard } from '@components/molecules'
 import { BaseLayout } from '@components/templates'
 
-const Home: NextPage = () => {
-  const onRedirectToDonate = () => {
-    Router.push('/donate')
-  }
-  const onRedirectToNeedUBI = () => {}
-
-  return (
-    <BaseLayout
-      title="#ProofOfIntegrity"
-      subTitle="A descentralized protocol to certify who is in need of Universal Basic Income"
-    >
-      <VStack flex={1} space={2}>
-        <Flex
-          alignItems="center"
-          justifyContent="center"
-          flexDirection={{ sm: 'column', md: 'row' }}
-          flex={3}
-        >
-          <ActionCard
-            onPress={onRedirectToNeedUBI}
-            title="I Need $UBI"
-            Icon={DollarSignIcon}
-            color="primary.700"
-            isDisabled
-            mr={{ base: 0, md: 300 }}
-            mb={{ base: 10, md: 0 }}
-          />
-          <Link href="/donate">
-            <ActionCard
-              onPress={onRedirectToDonate}
-              title="I Can Help"
-              Icon={HandShakeIcon}
-              color="secondary.900"
-            />
-          </Link>
-        </Flex>
-        <Text textAlign="center" flex={1} fontSize="lg">
-          A project to support Proof of Humanity initiative
-        </Text>
-      </VStack>
-    </BaseLayout>
-  )
-}
+const Home: NextPage = () => (
+  <BaseLayout
+    title="#ProofOfIntegrity"
+    subTitle="A descentralized protocol to certify who is in need of Universal Basic Income"
+  >
+    <Text textAlign="center" flex={1} fontSize="lg">
+      A project to support Proof of Humanity initiative
+    </Text>
+  </BaseLayout>
+)
 
 export default Home
